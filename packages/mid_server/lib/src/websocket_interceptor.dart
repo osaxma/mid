912c8edc47e3a79a9094106e016f2ca1ce9f4b41
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-19 00:50:56 +0300

    reorgnize files

---
+++ packages/mid_server/lib/src/websocket_interceptor.dart
@@ -0,0 +1,25 @@
+//  note: maybe passing the Request here isn't a good idea..
+//        we could create common object between http/websocket called ConnectionData
+//        where the package user can send data from client to server including auth if they wish too
+//        This will avoid relying on shelf package and the Request class and give the package more freedom
+//        in the future.
+import 'package:shelf/shelf.dart';
+
+import 'protocol.dart';
+
+/// Implement this class to create custom interceptor(s) for the websocket connection
+///
+///
+/// If [clientMessage] or [serverMessage] throws an exception, the connection will be
+/// terminated.
+abstract class MessageInterceptor {
+  /// Intercept messages sent by the client to the server
+  ///
+  /// The [request] is the initial request that was used to establish the connection
+  Message clientMessage(Message message, Request request);
+
+  /// Intercept messages sent by the server to the client
+  ///
+  /// The [request] is the initial request that was used to establish the connection
+  Message serverMessage(Message message, Request request);
+}
