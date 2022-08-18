60b6529bc5cce5965ba713ef5870793293bb25d8
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-19 00:30:36 +0300

    added mid server package

---
+++ packages/mid_server/README.md
@@ -0,0 +1,3 @@
+This package is not meant to be used on its own.
+
+Please visit [mid package](https://pub.dev/packages/mid) for more info. 
---
+++ packages/mid_server/lib/mid_server.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mid_server;
+
+export 'src/mid_server_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
