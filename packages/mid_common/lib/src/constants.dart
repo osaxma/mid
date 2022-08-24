dcd7fafceed4070a735332633b778f50baad85ba
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-24 22:14:22 +0300

    chore: added content type key constant

--- packages/mid_common/lib/src/constants.dart
+++ packages/mid_common/lib/src/constants.dart
@@ -1,2 +1,5 @@
 /// The key for the request id that's added to requests headers
 const requestIdKey = 'X-Request-ID';
+
+/// The key for the request content type that's added to requests headers
+const contentTypeKey = 'content-type';
