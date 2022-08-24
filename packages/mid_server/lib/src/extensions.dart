e28c1e3b93dd88eb073e603b81adc955d6a2dc97
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-24 22:32:18 +0300

    feat: added extensions to shelf Request & Response
    The extension enables retrieving the request id and also injecting it to the response

---
+++ packages/mid_server/lib/src/extensions.dart
@@ -0,0 +1,21 @@
+import 'package:mid_common/mid_common.dart';
+import 'package:shelf/shelf.dart';
+
+extension ResponsEx on Response {
+  /// inject the request id that came from the client request
+  Response injectRequestID(String id) {
+    return change(headers: {requestIdKey: id});
+  }
+
+  /// get the request id that came from the client request 
+  String? getRequestID() {
+    return headers[requestIdKey];
+  }
+}
+
+extension RequestEx on Request {
+  /// get the request id that came with the client side
+  String? getRequestID() {
+    return headers[requestIdKey];
+  }
+}
