6ac545b06e46a45635adb71ba9c74fcb5f2376aa
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-25 18:59:46 +0300

    chore: export interceptor

--- packages/mid_server/lib/mid_server.dart
+++ packages/mid_server/lib/mid_server.dart
@@ -2,6 +2,6 @@ library mid_server;
 
 export 'src/config.dart';
 export 'src/handlers.dart';
-// export 'src/router.dart';
+export 'src/interceptor.dart';
 export 'src/server.dart';
 export 'src/websocket_handler.dart';
