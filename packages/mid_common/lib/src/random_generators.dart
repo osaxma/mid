16a7fb90bdb666dc483f5674e028b437d8c0652e
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-25 15:25:04 +0300

    feat: added default value for random generator

--- packages/mid_common/lib/src/random_generators.dart
+++ packages/mid_common/lib/src/random_generators.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890-_';
 final _randomGenerator = Random();
-String generateRandomID(int length) => String.fromCharCodes(
+String generateRandomID([int length = 20]) => String.fromCharCodes(
       Iterable.generate(
         length,
         (_) => _chars.codeUnitAt(
