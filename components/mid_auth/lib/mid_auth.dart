87c0830c890a00751871c919f1691533356a1f29
osaxma <46427323+osaxma@users.noreply.github.com>
2022-08-26 16:14:54 +0300

    renamed auth_api to auth_endpoints

--- components/mid_auth/lib/mid_auth.dart
+++ components/mid_auth/lib/mid_auth.dart
@@ -6,5 +6,5 @@ export 'src/models/session.dart';
 export 'src/models/user_data.dart';
 export 'src/persistence/auth_db.dart';
 export 'src/persistence/sqlite/auth_sqlite.dart';
-export 'src/auth_api.dart';
+export 'src/auth_endpoints.dart';
 export 'src/exceptions.dart';
